3d4fc636ecb891be775a1d72e68d24abae2d9752
tulio.neto <tulio.jacinto32@gmail.com>
2025-02-06 11:59:05 -0300

    Atividades Quinta-Feira e correções Quarta

--- src/com/agibank/s2exemplo1/semana3/Quarta.java
+++ src/com/agibank/s2exemplo1/semana3/Quarta.java
@@ -2,6 +2,8 @@ package com.agibank.s2exemplo1.semana3;
 
 import com.agibank.s2exemplo1.semana2.Terca;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Quarta {
@@ -19,13 +21,13 @@ public class Quarta {
                 atv2();
                 break;
             case 3:
-//                atv3();
+                atv3();
                 break;
             case 4:
-//                atv4();
+                atv4();
                 break;
             case 5:
-//                atv5();
+                atv5();
                 break;
             default:
                 System.out.println("Opção inválida.[");
@@ -38,7 +40,7 @@ public class Quarta {
         int[][] matriz = new int[3][3];
 
         for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz.length; j++) {
+            for (int j = 0; j < matriz[0].length; j++) {
                 System.out.println("Digite o valor da tabela nos indices: " + i + " " + j);
                 matriz[i][j] = sc.nextInt();
             }
@@ -68,7 +70,7 @@ public class Quarta {
         for (int i = 0; i < matriz.length; i++) {
             somaLinha = 0;
             somaColuna = 0;
-            for (int j = 0; j < matriz.length; j++) {
+            for (int j = 0; j < matriz[0].length; j++) {
                 somaTotal += matriz[i][j];
                 somaLinha += matriz[i][j];
                 somaColuna += matriz[j][i];
@@ -78,4 +80,61 @@ public class Quarta {
         }
         System.out.println("somaTotal: " + somaTotal);
     }
+
+    public void atv3() {
+        int[][] matriz = {
+                {1,2,3},
+                {4,5,6}
+        };
+        for (int i = 0; i < matriz[0].length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(matriz[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void atv4() {
+        int[][] matriz = {
+                {1,2,3},
+                {4,5,6}
+        };
+        int[][] matriz2 = new int[matriz[0].length][matriz.length];
+        Random ram = new Random();
+        int num = ram.nextInt(5);
+        boolean find = false;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                if (matriz[i][j] == num) {
+                    System.out.printf("O Número: %d foi encontrado na posição: Linha %d, Coluna %d", num , i+1, j+1);
+                   find = true;
+                    break;
+                }
+                if (find) break;
+            }
+        }
+    }
+
+        public void atv5() {
+            int[][] matriz = {
+                    {1,2,3},
+                    {4,5,6}
+            };
+            int[][] matriz2 = {
+                    {1,2},
+                    {3,4},
+                    {5,6}
+            };
+            int[][] matriz3 = new int[matriz[0].length][matriz2.length];
+
+            for (int i = 0; i < matriz2.length; i++) {
+                for (int j = 0; j < matriz[0].length; j++) {
+                    if (i >= matriz.length || j >= matriz2[0].length) matriz3[i][j] = 0;
+                    else matriz3[i][j] = matriz[i][j] * matriz2[i][j];
+                }
+            }
+            for (int[] i : matriz3) System.out.println(Arrays.toString(i));
+        }
+
+
 }
--- src/com/agibank/s2exemplo1/semana3/Quinta.java
+++ src/com/agibank/s2exemplo1/semana3/Quinta.java
@@ -2,6 +2,7 @@ package com.agibank.s2exemplo1.semana3;
 
 import com.agibank.s2exemplo1.semana2.Terca;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Quinta {
@@ -13,23 +14,108 @@ public class Quinta {
 
         switch (vlr) {
             case 1:
-//                atv1();
+                atv1();
                 break;
             case 2:
-//                atv2();
+                atv2();
                 break;
             case 3:
-//                atv3();
+                atv3();
                 break;
             case 4:
-//                atv4();
+                atv4();
                 break;
             case 5:
-//                atv5();
+                atv5();
                 break;
             default:
                 System.out.println("Opção inválida.[");
                 break;
         }
+    }
+    public void atv1() {
+        //Receba uma matriz quadrada nxn e calcule a soma dos elementos da diagonal principal e da diagonal secundária.
+        int[][] matriz = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        int diagonalPrimaria = 0, diagonalSecundaria = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            diagonalPrimaria += matriz[i][i];
+            diagonalSecundaria += matriz[i][(matriz.length - 1) - i];
+        }
+
+        System.out.println("Soma da Diagonal Principal: " + diagonalPrimaria);
+        System.out.println("Soma da Diagonal Secundária: " + diagonalSecundaria);
+    }
+    public void atv2() {
+/*        Verifique se uma matriz qudrada nxn é uma matriz identidade.
+        Definição: uma matriz identidade possui ls na diagonal principal e 0s em todos os outros elementos
+    */
+        int[][] matriz = {
+                {1,0,0},
+                {0,1,0},
+                {0,0,1}
+        };
+        int cont = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                if (matriz[i][i] == 1 && matriz[i][j] != 0) {
+                    cont++;
+                }
+            }
+        }
+
+        if (cont == matriz.length) System.out.println("A matriz é uma identidade!");
+        else System.out.println("A matriz não é uma identidade!");
+
+    }
+    public void atv3() {
+//        Dada uma matriz nxm de números reais, normalize os valores, dividindo cada elemento pelo maior valor presente na matriz
+        double[][] matriz = {
+                {2.0, 4.0, 6.0},
+                {8.0, 10.0, 12.0}
+        };
+        double[][] matriz2 = new double[matriz.length][matriz[0].length];
+        double maior = matriz[0][0];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                if (maior < matriz[i][j]) maior = matriz[i][j];
+            }
+        }
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                matriz2[i][j] = matriz[i][j] / maior;
+            }
+        }
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.printf("%.2f ", matriz2[i][j]);
+            }
+            System.out.println(
+            );
+        }
+
+    }
+    public void atv4() {
+//            Implmente uma função para rotacionar uma matriz quadrada nxn em 90 graus no sentido horário
+        int[][] matriz = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = matriz[0].length - 1; j >= 0; j--) {
+                System.out.printf("%d ", matriz[j][i]);
+            }
+            System.out.println();
+        }
+    }
+    public void atv5() {
+
     }
 }
