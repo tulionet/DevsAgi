c27a9bd3bce165f2f0619dd8fe924cf38bd2cb2a
tulio.neto <tulio.jacinto32@gmail.com>
2025-02-10 23:00:54 -0300

    Correção exercicio de sexta feira maratona.

--- src/com/agibank/s2exemplo1/semana3/Sexta.java
+++ src/com/agibank/s2exemplo1/semana3/Sexta.java
@@ -1,5 +1,6 @@
 package com.agibank.s2exemplo1.semana3;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sexta {
@@ -29,96 +30,70 @@ public class Sexta {
                 break;
         }
     }
-/*    Nota:
 
-    Nome devs:
-    Tulio Neto, Bruno Henrique, Victoria rocha, Matheus Rocco, Carolina Guedes
-    Nome Tester:
-    */
     public void atv1() {
-        int idade = 30;
-        double contrMensal = 1000;
-        double taxaMensal = 0.01;
-        int totalAnos = 65;
-        int totalContribuicoes = totalAnos * 12;
-        double saldoFinal = 0;
-        int cont = 0;
-        double[][] taxasAnuais = {
-                {0, taxaMensal},
-                {20, taxaMensal + 0.03},
-                {40, taxaMensal + 0.05},
-                {60, taxaMensal + 0.07}
-        };
+        Scanner sc = new Scanner(System.in);
+        int idade = 0, idadeDesejadaAposentadoria = 0;
+        double taxaJurosAnual = 0, contribMensal = 0;
 
-        double[] saldoFinalAnos4 = new double[totalAnos];
-        double[] saldoFinalAnos6 = new double[totalAnos];
-        double[] saldoFinalAnos8 = new double[totalAnos];
+        System.out.print("Digite a sua idade: ");
+        idade = sc.nextInt();
+        System.out.print("Contribuição mensal para a aposentadoria: ");
+        contribMensal = sc.nextDouble();
+        System.out.print("Taxa de juros anual esperada: ");
+        taxaJurosAnual = sc.nextDouble();
+        System.out.print("Idade desejada para a aposentadoria: ");
+        idadeDesejadaAposentadoria = sc. nextInt();
+
+        System.out.println("Simulação de Previdência Privada\n");
+        System.out.printf("Idade atual: %d\n", idade);
+        System.out.printf("Contribuição mensal: R$ %.2f\n", contribMensal);
+        System.out.printf("Taxa de juros anual esperada: %.0f%%\n", taxaJurosAnual);
+        System.out.printf("Idade desejada para aposentadoria: %d anos\n\n", idadeDesejadaAposentadoria);
+
+        int totalAnos = idadeDesejadaAposentadoria - idade;
         double[] saldoFinalAnos = new double[totalAnos];
+        double taxaJurosMensal = (taxaJurosAnual / 100) / 12;
 
-        for (int i = 0; i < totalContribuicoes / 12; i++) {
-            int anoAtual = i / 12;
+        for (int i = 0; i < totalAnos ; i++) {
+            saldoFinalAnos[i] = contribMensal * (Math.pow(1+taxaJurosMensal, totalAnos*12)-1) / taxaJurosMensal;
+//            System.out.println("O ano " + i + " recebeu o valor: " + saldoFinalAnos[i]);
+        }
 
-            if (anoAtual >= taxasAnuais[3][0]) {
-                taxaMensal = taxasAnuais[3][1];
-            } else if (anoAtual >= taxasAnuais[2][0]) {
-                taxaMensal = taxasAnuais[2][1];
-            } else if (anoAtual >= taxasAnuais[1][0]) {
-                taxaMensal = taxasAnuais[1][1];
-            } else {
-                taxaMensal = taxasAnuais[0][1];
+        double[][] matriz = new double[4][totalAnos];
+        for (int i = 0; i < matriz.length; i++) {
+            double taxaJuros = 0;
+            if (i == 0) taxaJuros = 4;
+            else if (i == 1) taxaJuros = 6;
+            else if (i == 2) taxaJuros = 8;
+            else if (i == 3) taxaJuros = 10;
+            taxaJuros = (taxaJuros / 100) / 12;
+            for (int j = 0; j < matriz[0].length; j++) {
+                matriz[i][j] = contribMensal * (Math.pow(1+taxaJuros, totalAnos*12)-1) / taxaJuros;
             }
-
-            saldoFinal += (contrMensal * (Math.pow(1 + taxaMensal, 12) - 1) / taxaMensal);
-
-                saldoFinalAnos[cont] = saldoFinal;
-                cont++;
-
         }
+        System.out.println("\nSaldo final estimado!");
+        System.out.printf("Com taxa de 4%%: R$ %.2f\n", matriz[0][totalAnos-1]);
+        System.out.printf("Com taxa de 6%%: R$ %.2f\n", matriz[1][totalAnos-1]);
+        System.out.printf("Com taxa de 8%%: R$ %.2f\n", matriz[2][totalAnos-1]);
+        System.out.printf("Com taxa de 10%%: R$ %.2f\n", matriz[3][totalAnos-1]);
 
-            for (double i : saldoFinalAnos) {
-                System.out.println(i);
-            }
-
-        double saldoTotal4 = 0;
-        double saldoTotal6 = 0;
-        double saldoTotal8 = 0;
-
-        for (int i = 0; i < saldoFinalAnos.length; i++) {
-            saldoFinalAnos4[i] = saldoFinalAnos[i] * 1.04;
-            saldoFinalAnos6[i] = saldoFinalAnos[i] * 1.06;
-            saldoFinalAnos8[i] = saldoFinalAnos[i] * 1.08;
-        }
-
-        for (int i = 0; i < saldoFinalAnos.length; i++) {
-            saldoTotal4 += saldoFinalAnos4[i];
-            saldoTotal6 += saldoFinalAnos6[i];
-            saldoTotal8 += saldoFinalAnos8[i];
-        }
+        System.out.println("\nRenda mensal estimada para 20 anos de aposentadoria: ");
 
         int expectativaVida = 20;
-        int mesesDeVida = expectativaVida * 12;
+        double taxaJurosMensalidade = 0;
+        taxaJurosMensalidade = (4.0 / 100) / 12;
+        double rendaMensal = matriz[0][totalAnos-1] * (taxaJurosMensalidade / (1-(Math.pow(1+taxaJurosMensalidade, (-expectativaVida)*12))));
+        taxaJurosMensalidade = (6.0 / 100) / 12;
+        double rendaMensal6 = matriz[0][totalAnos-1] * (taxaJurosMensalidade / (1-(Math.pow(1+taxaJurosMensalidade, (-expectativaVida)*12))));
+        taxaJurosMensalidade = (8.0 / 100) / 12;
+        double rendaMensal8 = matriz[0][totalAnos-1] * (taxaJurosMensalidade / (1-(Math.pow(1+taxaJurosMensalidade, (-expectativaVida)*12))));
+        taxaJurosMensalidade = (10.0 / 100) / 12;
+        double rendaMensal10 = matriz[0][totalAnos-1] * (taxaJurosMensalidade / (1-(Math.pow(1+taxaJurosMensalidade, (-expectativaVida)*12))));
+        System.out.printf("Com taxa de 4%%: R$ %.2f\n", rendaMensal);
+        System.out.printf("Com taxa de 6%%: R$ %.2f\n", rendaMensal6);
+        System.out.printf("Com taxa de 8%%: R$ %.2f\n", rendaMensal8);
+        System.out.printf("Com taxa de 10%%: R$ %.2f\n", rendaMensal10);
 
-        double retiradaMensal4 = saldoTotal4 * (0.04 / (1 - Math.pow(1 + 0.04, -mesesDeVida)));
-        double retiradaMensal6 = saldoTotal6 * (0.06 / (1 - Math.pow(1 + 0.06, -mesesDeVida)));
-        double retiradaMensal8 = saldoTotal8 * (0.08 / (1 - Math.pow(1 + 0.08, -mesesDeVida)));
-
-        System.out.println("Idade atual: " + idade);
-
-        System.out.printf("Com taxa de 4%%: R$ %.2f%n", saldoTotal4);
-        System.out.printf("Renda mensal estimada (20 anos): R$ %.2f%n", retiradaMensal4);
-
-        System.out.printf("Com taxa de 6%%: R$ %.2f%n", saldoTotal6);
-        System.out.printf("Renda mensal estimada (20 anos): R$ %.2f%n", retiradaMensal6);
-
-        System.out.printf("Com taxa de 8%%: R$ %.2f%n", saldoTotal8);
-        System.out.printf("Renda mensal estimada (20 anos): R$ %.2f%n", retiradaMensal8);
     }
-
-
-
-
-
-
-
-
 }
