91a74d6285f40eb1979e6ea3e240fdddef6f750e
tulio.neto <tulio.jacinto32@gmail.com>
2025-03-06 08:20:13 -0300

    Exemplo de Programação orientada objetos (POO) criação da classe Carro e Main semana7.

---
+++ src/com/agibank/s2exemplo1/semana7/Carro.java
@@ -0,0 +1,42 @@
+package com.agibank.s2exemplo1.semana7;
+
+public class Carro {
+    private String marca;
+    private String modelo;
+    private int ano;
+
+    public Carro (String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public void acelerar() {
+        System.out.println("Acelerando");
+    }
+}
+
---
+++ src/com/agibank/s2exemplo1/semana7/Main.java
@@ -0,0 +1,10 @@
+package com.agibank.s2exemplo1.semana7;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+    Carro carro = new Carro("Renault", "Kwid", 2024);
+        carro.acelerar();
+    }
+}
